0a1687b7856aa3c80b7fd926b947baeebc7884aa
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2025-04-21 14:22:53 +0100

    feat: add claimGift functionality

--- contracts/GiftChain.sol
+++ contracts/GiftChain.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-contract GiftChain {
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+
+contract GiftChain is ReentrancyGuard{
+  using SafeERC20 for IERC20;
+
   struct Gift {
     address token;
     bool claimed;
@@ -19,5 +25,29 @@ contract GiftChain {
   }
 
   mapping (bytes32 => Gift) public gifts;
+
+  event GiftClaimed(bytes32 indexed code, address indexed recipient, uint256 amount);
+
+  function claimGift(bytes32 code) external nonReentrant {
+    Gift storage gift = gifts[code];
+
+    // gift must exist AND be in PENDING state (ready to claim)
+    require(gift.status == Status.PENDING, "Invalid gift code");
+
+    // gift must not be expired 
+    require(gift.expiry > block.timestamp, "Gift expired");
+
+    // validates the token and amount of gift
+    require(gift.token != address(0), "Invalid token");
+    require(gift.amount > 0, "Invalid amount");
+
+    //update status before transfer 
+    gift.status = Status.SUCCESSFUL; // gift successfully claimed
+
+    // transfer tokens
+    IERC20(gift.token).safeTransfer(msg.sender, gift.amount);
+
+    emit GiftClaimed(code, msg.sender, gift.amount);
+  }
 
 }
