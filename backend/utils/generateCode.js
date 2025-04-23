5b60d53d3d7e0a81665a6addda799aa867b491fd
fayob <adedejifavour6@gmail.com>
2025-04-23 02:33:22 +0100

    Add regex to code generation

--- backend/utils/generateCode.js
+++ backend/utils/generateCode.js
@@ -2,7 +2,8 @@ const crypto = require("crypto");
 const { keccak256, toUtf8Bytes } = require("ethers");
 
 function generateCode() {
-  const rawCode = crypto.randomBytes(8).toString("hex");
+  const randomCode = crypto.randomBytes(8).toString("hex");
+  const rawCode = randomCode.match(/.{1,4}/g).join("-");
   const hashedCode = keccak256(toUtf8Bytes(rawCode));
 
   return {
