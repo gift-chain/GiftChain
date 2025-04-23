afe4be3a6b9bf051e7046fc938d1b4ef49d474ab
fayob <adedejifavour6@gmail.com>
2025-04-24 00:33:32 +0100

    Add configuration for deploy and verify contract and fix some issues

--- SmartContract/hardhat.config.ts
+++ SmartContract/hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const {ALCHEMY_SEPOLIA_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_SEPOLIA_RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY!
+    }
+  }
 };
 
 export default config;
