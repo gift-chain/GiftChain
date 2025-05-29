4f3b6d125d39101b6ac7d1a91425eb9c54f8dfba
Mrwicks00 <victoradeoba83@gmail.com>
2025-05-29 01:34:28 +0100

    to open a pull request

--- FrontEnd/hooks/createConfig.ts
+++ FrontEnd/hooks/createConfig.ts
@@ -6,8 +6,7 @@ export const config = createConfig({
     chains: [mainnet, sepolia],
     connectors: [
         injected(), // This handles MetaMask and other injected wallets
-        metaMask(), // Specifically for MetaMask
-         // Only if you want WalletConnect support
+       
     ],
     transports: {
         [mainnet.id]: http(),
